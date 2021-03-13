388fb39a3e149022f448ddb6235a1efd2e8f3c2e
elhamj <ejcoms2006@gmail.com>
2021-03-13 23:05:43 +0330

    createElement added as jsx describing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    /*return (
       <div className="App">
         <h1>Hello World!!!</h1>
       </div>
-    );
+    );*/
+
+    // the above code is compiled to this, in the end.
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Compiled code!'));
   }
 }
 
