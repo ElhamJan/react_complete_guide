679f48ebde0b6305e1993de3f5cea23f976a9ffa
elhamj <ejcoms2006@gmail.com>
2021-03-17 21:22:24 +0330

    working with props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
-    /*return (
+    return (
       <div className="App">
         <h1>Hello World!!!</h1>
+        <Person name="Ali" age="32" />
+        <Person name="Elham" age="33">My hobbies: Reading</Person>
+        <Person name="Mami" age="67"/>
       </div>
-    );*/
+    );
 
     // the above code is compiled to this, in the end.
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Compiled code!'));
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'Compiled code!'));
   }
 }
 
