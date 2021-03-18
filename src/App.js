573e2bc8483368e2d925e56be0a100cffa136565
elhamj <ejcoms2006@gmail.com>
2021-03-18 22:20:39 +0330

    understanding state and handling events

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    persons : [
+      {name: 'Ali', age: '32'},
+      {name: 'Elham', age: '33'},
+      {name: 'Mami', age: '67'}
+    ],
+    otherThing: 'other value'
+  }
+
+  switchNameHandler = () => {
+    //Do not do this => this.state.persons[0].name= "Aliiiii";
+    this.setState({
+      persons : [
+        {name: 'Aliiiiii', age: '32'},
+        {name: 'Elham', age: '33'},
+        {name: 'Mami', age: '68'}
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World!!!</h1>
-        <Person name="Ali" age="32" />
-        <Person name="Elham" age="33">My hobbies: Reading</Person>
-        <Person name="Mami" age="67"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Reading</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
 
